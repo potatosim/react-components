0d0d0b56a9b66122935eb300fc2f89f7aadcfe52
Anna Yemelyanova <devildriver2310@gmail.com>
2023-03-28 21:52:28 +0300

    feat: rewrite FormPage, Modal, FormCards with FC

--- src/components/Cards/FormCards.tsx
+++ src/components/Cards/FormCards.tsx
@@ -1,35 +1,32 @@
 import FormCardItem from 'components/FormCardItem/FormCardItem';
-import React, { Component } from 'react';
+import { FC } from 'react';
 import { IFormCard } from 'types/IFormCard';
 import { uid } from 'uid';
 import styles from './Cards.module.scss';
 
-export default class FormCards extends Component<{ cards: IFormCard[] }> {
-  constructor(props: { cards: IFormCard[] }) {
-    super(props);
-  }
-
-  render() {
-    const { cards } = this.props;
-
-    if (!cards.length) {
-      return null;
-    }
-
-    return (
-      <div className={styles.cardsWrapper}>
-        {cards.map((card) => (
-          <FormCardItem
-            name={card.name}
-            birthday={card.birthday}
-            checkbox={card.checkbox}
-            image={card.image}
-            radio={card.radio}
-            selected={card.selected}
-            key={uid()}
-          />
-        ))}
-      </div>
-    );
-  }
+interface FormCardsProps {
+  cards: IFormCard[];
 }
+
+const FormCards: FC<FormCardsProps> = ({ cards }) => {
+  if (!cards.length) {
+    return null;
+  }
+  return (
+    <div className={styles.cardsWrapper}>
+      {cards.map((card) => (
+        <FormCardItem
+          name={card.name}
+          birthday={card.birthday}
+          checkbox={card.checkbox}
+          image={card.image}
+          radio={card.radio}
+          selected={card.selected}
+          key={uid()}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default FormCards;
